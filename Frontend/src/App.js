6fbf8bcb91eb240e1c68869cb78b17d51a5ebb63
Sami Räihä <sami.k.raiha@utu.fi>
2023-05-31 14:40:34 +0300

    Enhance styling of the navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,3 +1,4 @@
+import './app.css'
 import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './views/Home'
@@ -5,16 +6,18 @@ import JourneyList from './views/JourneyList'
 
 function App() {
   return (
-    <div className='app'>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/journeys'>Journey list</Link>
-          </li>
-        </ul>
+    <div className='app-root-container'>
+      <nav className='navbar'>
+        <div className='container'>
+            <ul className='navbar-list'>
+                <li className='navbar-item'>
+                    <Link to='/' className='navbar-link'>Home</Link>
+                </li>
+                <li className='navbar-item'>
+                    <Link to='/journeys' className='navbar-link'>Journey list</Link>
+                </li>
+            </ul>
+        </div>
       </nav>
 
       <Routes>
