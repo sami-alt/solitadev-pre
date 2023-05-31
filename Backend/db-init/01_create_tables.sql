ddfcf283e700444f5009d46d9831cce11616b28b
Sami Räihä <sami.k.raiha@utu.fi>
2023-05-31 23:06:41 +0300

    Change id column of station station data
    I originally thought that "fid" was the "main" identifier here,
    but seems that it is actually "id", as that is what the journey
    data uses to reference stations. Switch that and add an index in
    preparation for the query for single station.

--- Backend/db-init/01_create_tables.sql
+++ Backend/db-init/01_create_tables.sql
@@ -1,18 +1,6 @@
-create table journeys (
-    "id" int generated always as identity primary key,
-    departure timestamp not null,
-    "return" timestamp not null,
-    departure_station_id int not null,
-    departure_station_name text not null,
-    return_station_id int not null,
-    return_station_name text not null,
-    covered_distance float null,
-    duration_sec int not null
-);
-
 create table citybike_stations (
-    fid int not null primary key,
-    id int not null,
+    id int not null primary key,
+    fid int not null,
     name_fi text not null,
     name_se text not null,
     name_en text not null,
@@ -25,3 +13,21 @@ create table citybike_stations (
     x float not null,
     y float not null
 );
+
+create table journeys (
+    "id" int generated always as identity primary key,
+    departure timestamp not null,
+    "return" timestamp not null,
+    departure_station_id int not null,
+    departure_station_name text not null,
+    -- Note: normally we'd add "references citybike_stations" here,
+    -- but the journey data has references to stations that are missing
+    -- from the station data
+    return_station_id int not null,
+    return_station_name text not null,
+    covered_distance float null,
+    duration_sec int not null
+);
+
+create index journeys_departure_station_id on journeys ("departure_station_id");
+create index journeys_return_station_id on journeys ("return_station_id");
